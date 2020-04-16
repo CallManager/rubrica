d2ba85362f0373cc508b904b476c459a0908d5d2
Jonnycp <jonathan-caputo@hotmail.com>
2020-04-16 22:13:15 +0200

    -3kb

--- rubrica/options.js
+++ rubrica/options.js
@@ -1,18 +1,43 @@
+//FISICA
 document.getElementById("piattaformaFisica").addEventListener("change", getPlatFisica)
   function getPlatFisica() {
     var selectedPlatformFisica = document.getElementById("piattaformaFisica").value;
-    console.log(selectedPlatformFisica)
     if(selectedPlatformFisica === "Meet"){
     	document.getElementById("alertMeetFisica").setAttribute('style', 'display: block');
     }if(selectedPlatformFisica != "Meet"){
         document.getElementById("alertMeetFisica").setAttribute('style', 'display: none');
     };
   }
+//GET ID OF MEETING
+document.getElementById("saveButton1").addEventListener("click", save1)
+  function save1(){
+    var platformFisica = document.getElementById("piattaformaFisica").value;
+    var meetIdFisica = document.getElementById("codiceFisica").value;
+    var pwFisica = document.getElementById("pwFisica").value;
+    var fisica = [platformFisica, meetIdFisica, pwFisica];
+    chrome.storage.local.set({fisica: fisica}, function() {
+      console.log('Fisica salvato. Piattaforma:' + fisica[0] + ' ID:' + fisica[1] + ' Password:' + fisica[2])
+      document.getElementById("saveButton1").innerText = "Salvato!";
+    })
+  }
 
+//ALL SAVED DATES ARE DISPLAYED
+document.getElementById("richieste").addEventListener("click", save1)
+//function displayed() {
+chrome.storage.sync.get(['fisica'], function(result) {;
+  console.log(result.fisica [0]);
+});
+//  document.getElementById("piattaformaFisica").setAttribute('value', 'color: green');
+//}
 
-
-
-
+/* EASTER EGG XDXD
+var easterText = "";
+var easterI;
+for (easterI = 0; easterI < 420; easterI++) {
+  easterText += "Penis <br>";
+}
+document.getElementById("easterEggXddX").innerHTML = easterText;
+*/
 
 /*document.getElementById("zoomid").addEventListener("click", submit);
     function submit(){
